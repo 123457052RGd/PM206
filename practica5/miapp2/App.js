19ac9eec65fcce295111542a37814918bd223dd3
123457052RGd <123047921@upq.edu.mx>
2026-07-02 23:43:42 -0600

    Practica No.16: ActivityIndicator, KeyboardAvoidingView
    Se desarrolló una práctica guiada utilizando los componentes ActivityIndicator y KeyboardAvoidingView para comprender mejor su funcionamiento y aplicación en React Native en la practica guiada presentada

--- practica5/miapp2/App.js
+++ practica5/miapp2/App.js
@@ -1,42 +1,17 @@
-/*Zona1: Importaciones de componentes y archivos*/
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import MenuScreen from './screens/MenuScreen';
+import { View, Text } from 'react-native';
 
-
-
-
-/*Zona2: Main - */
 export default function App() {
   return (
-    <View style={styles.container}>
-      <MenuScreen/>
-
-      <StatusBar style="auto" />
-
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <Text>Hola Expo</Text>
     </View>
   );
 }
-
-
-/*Zona3: Estilos y posicionamientos*/
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-    flexDirection: 'center'
-  },
-  
-  tarjetaRoja: {
-    backgroundColor: '#adadb6'
-  },
-  tarjetaVerde: {
-    backgroundColor: '#6BCB77'
-  },
-});
-
-
 
 
