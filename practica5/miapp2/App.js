574121b5fad44987fb27510ee1986586f1ddba54
123457052RGd <123047921@upq.edu.mx>
2026-07-04 20:59:18 -0600

    Practica No.17: Repaso de componentes II
    En esta práctica aplicamos todos los conocimientos aprendidos hasta el momento para comprender mejor el funcionamiento de los componentes de React Native. Además, aprendimos cómo estos componentes permiten crear una interfaz atractiva, organizada y funcional, contribuyendo al desarrollo de una aplicación con una buena apariencia y una mejor experiencia para el usuario.

--- practica5/miapp2/App.js
+++ practica5/miapp2/App.js
@@ -1,17 +1,75 @@
-import { View, Text } from 'react-native';
+/*Zona1: Importaciones de componentes y archivos*/
+
+import { StatusBar } from 'expo-status-bar';
+
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import MenuScreen from './screens/MenuScreen';
+
+
+
+
+
+
+
+
+
+/*Zona2: Main - */
 
 export default function App() {
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
-    >
-      <Text>Hola Expo</Text>
+
+    <View style={styles.container}>
+
+      <MenuScreen/>
+
+
+
+      <StatusBar style="auto" />
+
+
+
     </View>
+
   );
+
 }
 
 
+
+
+
+/*Zona3: Estilos y posicionamientos*/
+
+const styles = StyleSheet.create({
+
+  container: {
+
+    flex: 1,
+
+    backgroundColor: '#fff',
+
+    alignItems: 'center',
+
+    justifyContent: 'space-evenly',
+
+    flexDirection: 'center'
+
+  },
+
+  
+
+  tarjetaRoja: {
+
+    backgroundColor: '#adadb6'
+
+  },
+
+  tarjetaVerde: {
+
+    backgroundColor: '#6BCB77'
+
+  },
+
+});
